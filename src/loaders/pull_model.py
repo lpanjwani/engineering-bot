a36b521d21f88fceecb84944359cd02f389dfac7
Lavesh <lpanjwani@outlook.com>
2023-11-13 03:42:07 +0400

    Update Dockerfile and environment variables

--- src/loaders/pull_model.py
+++ src/loaders/pull_model.py
@@ -3,21 +3,20 @@ import os
 
 import requests
 
-model_name = os.getenv(
+MODEL_NAME = os.getenv(
     "LLM_MODEL",
 )
-OLLAMA_API_HOST = os.getenv("OLLAMA_API_HOST", "localhost")
+OLLAMA_API_HOST = os.getenv(
+    "OLLAMA_API_HOST",
+)
 
-print(f"Using Model: {model_name}")
+print(f"Using Model: {MODEL_NAME}")
 print(f"Using Ollama Base URL: {OLLAMA_API_HOST}")
 
 
-def pull_model(model_name):
-    print(f"pulling Model '{model_name}'...")
+def pull_model():
+    print(f"pulling Model '{MODEL_NAME}'...")
     url = f"http://{OLLAMA_API_HOST}:11434/api/pull"
-    data = json.dumps(dict(name=model_name))
+    data = json.dumps(dict(name=MODEL_NAME))
     headers = {"Content-Type": "application/json"}
     requests.post(url, data=data, headers=headers)
-
-
-pull_model(model_name=model_name)
